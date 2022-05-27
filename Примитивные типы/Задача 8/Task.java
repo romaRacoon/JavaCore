5098e7ae4d1c4e73ef40e95fec02c23c8d4916e2
romaRacoon <roma1808@inbox.ru>
2022-05-27 18:12:57 +0300

    Create Task.java

---
+++ Примитивные типы/Задача 8/Task.java
@@ -0,0 +1,14 @@
+import java.math.BigInteger;
+
+public class Task {
+    public static void main(String[] args) {
+        System.out.println(factorial(0));
+    }
+    public static BigInteger factorial(int value) {
+        if (value <= 1) {
+            return new BigInteger(Integer.toString(1));
+        } else {
+            return BigInteger.valueOf(value).multiply(factorial(value - 1));
+        }
+    }
+}
