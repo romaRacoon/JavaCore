550a2f8c5193c9718736de70dee96d634b79ca3d
romaRacoon <roma1808@inbox.ru>
2022-05-27 22:27:11 +0300

    Create Task.java

---
+++ Примитивные типы/Задача 11/Task.java
@@ -0,0 +1,33 @@
+import java.math.BigInteger;
+
+public class Task {
+    public static void main(String[] args) {
+
+    }
+    public static int[] mergeArrays(int[] a1, int[] a2) {
+        int[] sortedArray = new int[a1.length + a2.length];
+        int i = 0;
+        int j = 0;
+        for (int k = 0; k < sortedArray.length; k++) {
+            if (i > a1.length - 1) {
+                int a = a2[j];
+                sortedArray[k] = a;
+                j++;
+            } else if (j > a2.length - 1) {
+                int a = a1[i];
+                sortedArray[k] = a;
+                i++;
+            } else if (a1[i] < a2[j]) {
+                int a = a1[i];
+                sortedArray[k] = a;
+                i++;
+            } else {
+                int b = a2[j];
+                sortedArray[k] = b;
+                j++;
+            }
+        }
+
+        return sortedArray;
+    }
+}
