6fa9c1e8b23f8697dcbd6401163479a23f38aae0
romaRacoon <roma1808@inbox.ru>
2022-05-29 02:11:55 +0300

    Create Test.java

---
+++ Объекты, классы и пакеты в Java/Задача 4/Test.java
@@ -0,0 +1,50 @@
+public class Test {
+    public static void main(String[] args) {
+        double a = 3.4;
+        int b = (int) a;
+        System.out.println(b);
+    }
+    public static class ComplexNumber {
+        private double re;
+        private double im;
+
+        public ComplexNumber() {
+        }
+
+        public ComplexNumber(double re, double im) {
+            this.re = re;
+            this.im = im;
+        }
+
+        public double getRe() {
+            return re;
+        }
+
+        public double getIm() {
+            return im;
+        }
+
+        @Override
+        public boolean equals(Object object) {
+            if (this == object) {
+                return true;
+            }
+            if (object == null || getClass() != object.getClass()) {
+                return false;
+            }
+            ComplexNumber complexNumber = (ComplexNumber) object;
+            return re == complexNumber.getRe() && im == complexNumber.getIm();
+        }
+
+        @Override
+        public int hashCode() {
+            int result;
+            long temp;
+            temp = Double.doubleToLongBits(re);
+            result = (int) (temp ^ (temp >>> 32));
+            temp = Double.doubleToLongBits(im);
+            result = 31 * result + (int) (temp ^ (temp >>> 32));
+            return result;
+        }
+    }
+}
