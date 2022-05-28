6225930fca40ac27aab1ea79c11df0c6a3fb23c0
romaRacoon <roma1808@inbox.ru>
2022-05-28 19:31:56 +0300

    Create Test.java

---
+++ Объекты, классы и пакеты в Java/Задача 2/Test.java
@@ -0,0 +1,28 @@
+public class Test {
+    public static void main(String[] args) {
+        Human human = new Human();
+        Human human1 = new Human((byte)19, "Рома", "Артеменков", "Бокс");
+        Human human2 = new Human((byte)19, "Рома", "Артеменков");
+    }
+    public static class Human {
+        private byte age;
+        private String name;
+        private String secondName;
+        private String favoriteSport;
+
+        public Human() {
+
+        }
+        public Human(byte age, String name, String secondName, String favoriteSport) {
+            this.age = age;
+            this.name = name;
+            this.secondName = secondName;
+            this.favoriteSport = favoriteSport;
+        }
+        public Human(byte age, String name, String secondName) {
+            this.age = age;
+            this.name = name;
+            this.secondName = secondName;
+        }
+    }
+}
