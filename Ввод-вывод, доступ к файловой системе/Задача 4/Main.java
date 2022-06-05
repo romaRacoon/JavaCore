10e204aeab531f0b4e09db6df28dcb71dc5cd5e4
romaRacoon <roma1808@inbox.ru>
2022-06-05 19:40:26 +0300

    Update Main.java

--- Ввод-вывод, доступ к файловой системе/Задача 4/Main.java
+++ Ввод-вывод, доступ к файловой системе/Задача 4/Main.java
@@ -2,29 +2,14 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        ArrayList<String> numbers = new ArrayList<String>();
-        while (scanner.hasNext()) {
-            String input = scanner.nextLine().trim();
-            numbers.add(input);
-        }
         double sum = 0;
-        if (numbers.size() == 1) {
-            String[] stringNumbers = numbers.get(0).split("\s");
-            for (int i = 0; i < stringNumbers.length; i++) {
-                try {
-                    sum += Double.parseDouble(stringNumbers[i]);
-                } catch (Exception e) {
-                }
+        try (Scanner scanner = new Scanner(System.in).useLocale(Locale.US)) {
+            while (scanner.hasNext()) {
+                String input = scanner.nextLine();
             }
-        } else {
-            for (int i = 0; i < numbers.size(); i++) {
-                try {
-                    sum += Double.parseDouble(numbers.get(i));
-                } catch (Exception e){
-                }
-            }
+        } catch (Exception e) {
+            System.out.printf("%f", 0);
         }
-        System.out.printf("%f", sum);
+        System.out.printf("%.6f", sum);
     }
 }
