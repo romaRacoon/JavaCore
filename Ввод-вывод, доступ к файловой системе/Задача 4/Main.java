15f0ce1b7a09671568877ed99091cea1553e0614
romaRacoon <roma1808@inbox.ru>
2022-06-05 16:33:15 +0300

    Update Main.java

--- Ввод-вывод, доступ к файловой системе/Задача 4/Main.java
+++ Ввод-вывод, доступ к файловой системе/Задача 4/Main.java
@@ -1,27 +1,64 @@
 import java.io.*;
 import java.nio.*;
+import java.text.DecimalFormat;
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        ArrayList<Double> trueNumbers = new ArrayList<Double>();
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         String input = reader.readLine();
         String[] numbers = getAllNumbers(input);
+        if (checkAllNumbersWithoutLetter(numbers) == false) {
+            System.out.println("0.000000");
+            System.exit(0);
+        } else {
+            for (int i = 0; i < numbers.length; i++) {
+                trueNumbers.add(Double.parseDouble(numbers[i]));
+            }
+            getSum(trueNumbers);
+        }
     }
     private static String[] getAllNumbers(String input) {
         String[] numbers = input.split("\s");
         return numbers;
     }
     private static boolean isInputWithoutLetter(String input) {
-        return input.indexOf("\\w") == -1;
+        int index = input.indexOf("\\w");
+        if (index != -1) {
+            return input.charAt(index) == '.';
+        } else {
+            if (findLetter(input)) {
+                return false;
+            } else {
+                return true;
+            }
+        }
     }
     private static boolean checkAllNumbersWithoutLetter(String[] numbers) {
         for (int i = 0; i < numbers.length; i++) {
-            if (isInputWithoutLetter(numbers[i])) {
-                //Тут должен выводится нулевой дабл
+            if (isInputWithoutLetter(numbers[i]) == false) {
                 return false;
             }
         }
         return true;
+    }
+    private static void getSum(ArrayList<Double> trueNumbers) {
+        double sum = 0;
+        for (int i = 0; i < trueNumbers.size(); i++) {
+            sum += trueNumbers.get(i);
+        }
+        System.out.println(sum);
+    }
+    private static boolean findLetter(String input) {
+        String alphabet = "abcdefghijklmnopqrstuvwxyz";
+        for (int i = 0; i < input.length(); i++) {
+            for (int j = 0; j < alphabet.length(); j++) {
+                if (input.toLowerCase().charAt(i) == alphabet.charAt(j)) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
