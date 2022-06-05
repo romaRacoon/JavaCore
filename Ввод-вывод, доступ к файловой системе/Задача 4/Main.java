1abf34caf32cf206bfb55d64e2815f473028544f
romaRacoon <roma1808@inbox.ru>
2022-06-05 18:43:53 +0300

    Update Main.java

--- Ввод-вывод, доступ к файловой системе/Задача 4/Main.java
+++ Ввод-вывод, доступ к файловой системе/Задача 4/Main.java
@@ -3,61 +3,17 @@ import java.math.BigDecimal;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        ArrayList<Double> trueNumbers = new ArrayList<Double>();
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        reader.readLine();
-        
-    }
-    private static String[] getAllNumbers(String input) {
-        String[] numbers = input.split("\s");
-        return numbers;
-    }
-    private static boolean isInputWithoutLetter(String input) {
-        int index = input.indexOf("\\w");
-        if (index != -1) {
-            return input.charAt(index) == '.';
-        } else {
-            if (findLetter(input)) {
-                return false;
-            } else {
-                return true;
-            }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<String> numbers = new ArrayList<String>();
+        while (scanner.hasNext()) {
+            String input = scanner.nextLine();
+            numbers.add(input.trim());
         }
-    }
-    private static boolean checkAllNumbersWithoutLetter(String[] numbers) {
-        for (int i = 0; i < numbers.length; i++) {
-            if (isInputWithoutLetter(numbers[i]) == false) {
-                return false;
-            }
-        }
-        return true;
-    }
-    private static void getSum(ArrayList<Double> trueNumbers) {
         double sum = 0;
-        for (int i = 0; i < trueNumbers.size(); i++) {
-            sum += trueNumbers.get(i);
+        for (int i = 0; i < numbers.size(); i++) {
+            sum += Double.parseDouble(numbers.get(i));
         }
-        String number = Double.toString(sum);
-        int amount = new BigDecimal(number).scale();
-        if (amount == 6) {
-            System.out.println(sum);
-        } else if (amount < 6) {
-            for (int i = 0; i < 6 - amount; i++) {
-                number += "0";
-            }
-            System.out.println(number);
-        }
-    }
-    private static boolean findLetter(String input) {
-        String alphabet = "abcdefghijklmnopqrstuvwxyz";
-        for (int i = 0; i < input.length(); i++) {
-            for (int j = 0; j < alphabet.length(); j++) {
-                if (input.toLowerCase().charAt(i) == alphabet.charAt(j)) {
-                    return true;
-                }
-            }
-        }
-        return false;
+        System.out.printf("%f", sum);
     }
 }
