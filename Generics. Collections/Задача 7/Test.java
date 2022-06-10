ecaa19db6c1fbe032215d86e00043ed38d0c9e00
romaRacoon <roma1808@inbox.ru>
2022-06-11 00:58:08 +0300

    Update Test.java

--- Generics. Collections/Задача 7/Test.java
+++ Generics. Collections/Задача 7/Test.java
@@ -3,16 +3,15 @@ import java.util.*;
 public class Test {
     public static void main(String[] args) {
         int counter = 1;
+        String input = "";
         Deque<Integer> deque = new ArrayDeque<>();
         Scanner scanner = new Scanner(System.in);
         while (scanner.hasNext()) {
-            scanner.next();
-            if (scanner.hasNextInt()) {
-                if (counter % 2 == 0) {
-                    deque.add(Integer.parseInt(String.valueOf(scanner.next())));
-                }
-                counter++;
+            input = scanner.next();
+            if (counter % 2 == 0) {
+                deque.addFirst(Integer.parseInt(input));
             }
+            counter++;
         }
         for (Integer value : deque) {
             System.out.print(value + " ");
