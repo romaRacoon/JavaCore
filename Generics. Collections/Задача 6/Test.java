57b3f864b29f8551a8dbe0e4493e6c260893d8f5
romaRacoon <roma1808@inbox.ru>
2022-06-12 19:08:57 +0300

    Update Test.java

--- Generics. Collections/Задача 6/Test.java
+++ Generics. Collections/Задача 6/Test.java
@@ -1,16 +1,9 @@
-import java.io.BufferedReader;
-import java.io.ByteArrayInputStream;
-import java.io.InputStreamReader;
 import java.io.Reader;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 public class Test {
     public static void main(String[] args) {
-        ByteArrayInputStream bais = new ByteArrayInputStream("Алексей 3000\nДмитрий 9000\nАнтон 3000\nАлексей 7000\nАнтон 8000".getBytes());
-        BufferedReader buffReader = new BufferedReader(new InputStreamReader(bais));
-        System.out.println(getSalesMap(buffReader));;
+
     }
     public static Map<String, Long> getSalesMap(Reader reader) {
         Map<String, Long> myMap = new HashMap<>();
@@ -18,11 +11,7 @@ public class Test {
         while (scanner.hasNext()) {
             String letterForName = scanner.next();
             Long letterForAmount = Long.valueOf(scanner.nextLong());
-            if (myMap.containsKey(letterForName)) {
-                myMap.put(letterForName, myMap.get(letterForName) + letterForAmount);
-            } else {
-                myMap.put(letterForName, letterForAmount);
-            }
+            myMap.merge(letterForName, letterForAmount, Long::sum);
         }
         return  myMap;
     }
