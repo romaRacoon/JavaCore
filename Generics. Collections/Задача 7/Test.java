fa40d83dbd859d0a41846a0b6b901e7798cbd79c
romaRacoon <roma1808@inbox.ru>
2022-06-12 19:11:31 +0300

    Update Test.java

--- Generics. Collections/Задача 7/Test.java
+++ Generics. Collections/Задача 7/Test.java
@@ -1,17 +1,15 @@
+import java.io.Reader;
 import java.util.*;
 
 public class Test {
     public static void main(String[] args) {
-        int counter = 1;
-        String input = "";
         Deque<Integer> deque = new ArrayDeque<>();
         Scanner scanner = new Scanner(System.in);
-        while (scanner.hasNext()) {
-            input = scanner.next();
-            if (counter % 2 == 0) {
-                deque.addFirst(Integer.parseInt(input));
+        while (scanner.hasNextInt()) {
+            scanner.next();
+            if (scanner.hasNextInt()) {
+                deque.addFirst(scanner.nextInt());
             }
-            counter++;
         }
         for (Integer value : deque) {
             System.out.print(value + " ");
