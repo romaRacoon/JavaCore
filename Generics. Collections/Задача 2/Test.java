7587dc48e073fac07b14cf41db7d2f38ea57ca00
romaRacoon <roma1808@inbox.ru>
2022-06-13 16:22:41 +0300

    Update Test.java

--- Generics. Collections/Задача 2/Test.java
+++ Generics. Collections/Задача 2/Test.java
@@ -1,14 +1,12 @@
-import java.util.*;
-import java.util.function.UnaryOperator;
+
 
 public class Test {
     public static void main(String[] args) {
-
     }
     public static class Box<T> {
         private T object;
         public static <T> Box <T> getBox() {
-            return new <T> Box();
+            return new Box<T>();
         }
     }
 }
