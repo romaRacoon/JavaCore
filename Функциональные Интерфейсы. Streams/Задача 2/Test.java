9939649c36c789871e04523348281c18ad5cf537
romaRacoon <roma1808@inbox.ru>
2022-06-14 19:53:45 +0300

    Update Test.java

--- Функциональные Интерфейсы. Streams/Задача 2/Test.java
+++ Функциональные Интерфейсы. Streams/Задача 2/Test.java
@@ -1,4 +1,3 @@
-import java.util.*;
 import java.util.function.UnaryOperator;
 
 public class Test {
@@ -8,8 +7,8 @@ public class Test {
     public static interface NumberGenerator<T extends Number> {
         boolean cond(T arg);
     }
+
     public static NumberGenerator<? super Number> getGenerator() {
-        NumberGenerator<Number> numberGenerator = x -> x.intValue() > 0;
-        return numberGenerator;
+        return x -> x.intValue() > 0;
     }
 }
